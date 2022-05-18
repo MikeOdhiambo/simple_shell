a1cb76130bbafcf69ac8fc6547a954da30e02780
Mike Odhiambo <99796816+MikeOdhiambo@users.noreply.github.com>
2022-05-18 16:55:25 +0300

    Update hsh.h

--- hsh.h
+++ hsh.h
@@ -33,19 +33,23 @@ quote_state_t _read_input(char **lineptr, int fd);
 /* Process user input */
 int parse(info_t *info);
 
+/* Execute command */
 int execute(info_t *info);
 int _execute(info_t *info);
 
+/* Expand and handle alises */
 void expand_aliases(alias_t *aliases, char ***tokptr);
 char *expand_alias(alias_t *aliases, char ***tokptr);
 
 void expand_vars(info_t *info, char ***tokptr);
 char **_expand_vars(info_t *info, char ***tokptr);
 
+/* Remove comments */
 void remove_comments(cmdlist_t *cmd);
 
 void open_script(info_t *info);
 
+/* Check for signals */
 void _sigint(int signal);
 
 #endif /* SHELL_H */
